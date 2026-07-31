3bc13d42f8feb6b97a5efaca217ea4cf8246774d
samarthmulagund <samarth.mulagund2014@gmail.com>
2026-07-31 16:56:30 +0530

    Add files via upload

--- Python_Programs/percentage_calculator.py
+++ Python_Programs/percentage_calculator.py
@@ -6,7 +6,7 @@ def get_base64(image_file):
         return base64.b64encode(f.read()).decode()
 st.set_page_config(
     page_title="Percentage Calc",
-    page_icon="%"
+    page_icon="📊"
 )
 
 img = get_base64("Python_Programs/images/calc-backdrop.jpeg")
